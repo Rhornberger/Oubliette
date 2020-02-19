18ede97f98bc4eb168a5d6adc1b4d121171226c7
Rhi <rhiraed@gmail.com>
2020-02-19 15:27:51 -0800

    removed unwanted files this was done to declutter the repo

--- things-i-might-keep/oubliette/about.vue.js
+++
@@ -1,8 +0,0 @@
-var About = {
-    template:     
-        `<div>
-            <div>   
-                <p>Here at Oubliette we believe that Tabletop RPG's shouldn't leave you buried under stacks of books. Your time is much better spent making progress in your adventure not leafing though hundreds of pages of rule books to find the information you need. We strive to make your game playing experience as pleasent and non cluttered as possible.</p>
-            </div>
-        </div>`
-}
